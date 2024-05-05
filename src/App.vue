07136b7565e1241bfefa1045491a2c2c74114784
rkrshen <rkr.shen@1177tech.com.tw>
2024-05-05 21:38:07 +0800

    d

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@ onMounted(() => {
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld :msg="mainProfile" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 defineProps<{
-  msg: string
+  msg: string | object
 }>()
 </script>
 
