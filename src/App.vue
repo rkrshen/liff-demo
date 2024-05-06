c9a94b5362ba9f27eba00319cf87b6ae01ab6e01
rkrshen <rkr.shen@1177tech.com.tw>
2024-05-06 09:13:06 +0800

    3

--- src/App.vue
+++ src/App.vue
@@ -1,114 +1,65 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import liff from '@line/liff'
-import { onMounted, ref } from 'vue';
-
-const mainProfile = ref({})
-onMounted(() => {
-  liff
-    .init({ liffId: '2004824985-x3R0k5ow' })
-    .then(() => {
-      // if (!liff.isLoggedIn()) {
-      //   console.log('not login');
-      //   liff.login();
-      // } else {
-      //   liff.getProfile().then((profile) => {
-      //     console.log('displayName', profile.displayName);
-      //     console.log('userId', profile.userId);
-      //     console.log('pictureUrl', profile.pictureUrl);
-      //     console.log('statusMessage', profile.statusMessage);
-      //   });
-      // }
-      liff.getProfile().then((profile) => mainProfile.value = profile);
-      console.log(liff.getOS())
-      console.log(liff.getLanguage())
-      console.log(liff.getVersion())
-    })
-    .catch((err) => {
-      console.log(err.code, err.message);
-    });
-})
-
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div v-if="!isLogin">
+    <h1>create-liff-app</h1>
+    <p v-if="message">{{ message }}</p>
+    <p v-if="error">
+      <code>{{ error }}</code>
+    </p>
+    <a href="https://developers.line.biz/ja/docs/liff/" target="_blank" rel="noreferrer">
+      LIFF Documentation
+    </a>
+  </div>
+  <div v-else>
 
-    <div class="wrapper">
-      <HelloWorld :msg="mainProfile" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script lang="ts">
+import { defineComponent } from "vue";
+import liff from "@line/liff";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default defineComponent({
+  data() {
+    return {
+      message: "",
+      error: "",
+      isLogin: false,
+      profile: {},
+    };
+  },
+  mounted() {
+    liff
+      .init({
+        liffId: "2004827358-P3mE3j8E",
+        withLoginOnExternalBrowser: true,
+      })
+      .then(() => {
+        this.message = "LIFF init succeeded.";
+        if(liff.isLoggedIn()) {
+          this.message += " You are logged in.";
+          this.isLogin = true;
+          this.profile = liff.getProfile();
+        } else {
+          this.message += " You are not logged in.";
+          liff.login({ redirectUri: "https://localhost:5173" });
+        }
+      })
+      .catch((e: Error) => {
+        this.message = "LIFF init failed.";
+        this.error = `${e}`;
+      });
+  }
+});
+</script>
 
-nav {
-  width: 100%;
-  font-size: 12px;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
